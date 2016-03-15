d171a06abfdcd98e20868ce6ef9833223cbec439
Aishwarya <aishwaryashilpi5395@gmail.com>
2016-03-15 12:44:00 +0530

    Initialize repo

---
+++ server.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var app = express();
+var PORT = 3000;
+//creating routes
+//ADDING MIDDLEWARE
+var middleware = {
+	requireAuthentication: function(req, res, next){
+		console.log('private route hit');
+		next();
+	},
+	logger: function(req, res, next){
+		// new Date().toString()
+		console.log('Request: '+new Date().toString()+' '+req.method+' '+req.originalUrl);
+		next();
+	}
+};
+
+app.use(middleware.requireAuthentication);
+
+app.use(middleware.logger);
+app.get('/about'/*, middleware.requireAuthentication*/,function(req,res){
+	res.send('About Us');
+});
+
+// app.get('/',function(req,res){
+// 	res.send('Hello Express');
+// });
+
+app.use(express.static(__dirname+'/public'));
+//console.log(__dirname);
+
+app.listen(PORT,function(){
+	console.log('Express server started on port: '+PORT);
+});
